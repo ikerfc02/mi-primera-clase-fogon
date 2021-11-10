e0f4bc241ecc984e713d5ea34596169d5b6953c2
annttonnio <antonio56.lop@gmail.com>
2021-11-10 16:50:09 +0100

    01- Commit Inicial

---
+++ Fogon.java
@@ -0,0 +1,72 @@
+public class Fogon {
+    //quien cocina
+    private String Cocinero;
+    //grados del fogon
+    private int grados;
+    //fogon encendido o no
+    private boolean encendido;
+    
+    public Fogon (String cocineroActual){
+        Cocinero = cocineroActual;
+        grados = 0;      
+    }
+       
+    public void encenderFogon(){
+        encendido = true;
+    }
+    
+    public void apagarFogon(){
+        encendido = false;
+        grados = 0;
+    }
+    
+    public boolean encendido () {
+        return encendido;
+    }
+    
+    public void setGrados(int gradosDelFogon){
+        grados = gradosDelFogon;
+    }
+    
+    public void setCocinero(String cocineroDelFogon){
+        Cocinero = cocineroDelFogon;
+    }
+
+    public void imprimeDatos () {
+        System.out.println (" Cocinero " + (Cocinero) +". "+ " Temperatura del fogón " + (grados)
+        + " grados. " + "Fogon encendido: " + (encendido)   );
+    }
+     
+    public String returnDatos () {
+        String estadoFogon;
+            estadoFogon = ("Cocinero " + (Cocinero) + " Temperatura del fogón " + (grados)
+        + " grados. " + " Fogon encendido: " + (encendido)  ); 
+        return estadoFogon; 
+        
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
